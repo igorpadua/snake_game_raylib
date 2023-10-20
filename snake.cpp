8ce8071e10a21d25ac5b8b8796d360c0453e1bb2
igorpadua <igorpadua@tutanota.com>
2023-10-20 15:01:18 -0300

    feat: snake arredondada

--- snake.cpp
+++ snake.cpp
@@ -13,6 +13,7 @@ auto Snake::draw() -> void
     for (auto i = 0u; i < body.size(); ++i) {
         auto x = body[i].x;
         auto y = body[i].y;
-        DrawRectangle(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, MY_DARK_GREEN);
+        auto segment = Rectangle{x * CELL_SIZE,  y * CELL_SIZE, static_cast<float>(CELL_SIZE), static_cast<float>(CELL_SIZE)};
+        DrawRectangleRounded(segment, 0.5, 6, MY_DARK_GREEN);
     }
 }
