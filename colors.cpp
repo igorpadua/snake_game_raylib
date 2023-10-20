dbf92896520710c58bdaee0afc13d30636baaf44
igorpadua <igorpadua@tutanota.com>
2023-10-20 11:35:09 -0300

    refactor: colors

---
+++ colors.cpp
@@ -0,0 +1,4 @@
+#include "colors.hpp"
+
+const Color MY_GREEN = {173, 204, 96, 255};
+const Color MY_DARK_GREEN = {43, 51, 24, 255};
---
+++ colors.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "raylib.h"
+
+extern const Color MY_GREEN;
+extern const Color MY_DARK_GREEN;
